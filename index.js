a6bfdc15ac8f10d43e530c9371b8bcbafefdf96f
João Pedro <pedro.munhoz98@gmail.com>
2020-11-28 17:44:28 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var parser = require("fast-xml-parser");
+const request = require("request");
+
+const adress = 11707310;
+
+const url = "http://viacep.com.br/ws/";
+const path = `${adress}/xml/`;
+
+request(`${url}${path}`, (err, res, body) => {
+  var { xmlcep } = parser.parse(body);
+
+  console.log(
+    `${xmlcep.logradouro}, ${xmlcep.bairro} - ${xmlcep.localidade}/${xmlcep.uf}`
+  );
+});
